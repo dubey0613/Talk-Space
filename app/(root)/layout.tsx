67607521dc803042e555e73553fd27da1ce7c306
dubey0613 <dubey.sarthak0613@gmail.com>
2024-05-30 18:29:12 +0530

    final commit with all the files!

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,23 @@
+import StreamVideoProvider from '@/providers/StreamClientProvider'
+import React, { ReactNode } from 'react'
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "TalkSpace",
+  description: "Video Calling Application",
+  icons: {
+    icon: '/icons/logo.png'
+  }
+};
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main>
+      <StreamVideoProvider>
+        {children}
+      </StreamVideoProvider>
+    </main>
+  )
+}
+
+export default RootLayout
